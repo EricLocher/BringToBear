38e4245d4fe83f4d2bc4c9515bb8d4c2735dfc09
EricL <eriloc1129@skola.goteborg.se>
2021-11-23 10:33:55 +0100

    Added: Folders and subfolders

---
+++ BringToBear/Assets/Scripts/Player/IEntity.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEntity {
+
+    public void Damage();
+
+
+}
---
+++ BringToBear/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IEntity
+{
+    public void Damage()
+    {
+
+    }
+
+
+}
