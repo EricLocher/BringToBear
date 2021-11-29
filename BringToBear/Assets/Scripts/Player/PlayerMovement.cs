9ce679182f65ca6e2d5bee19a4e8ef9951047378
PepperRoni <90191976+PepperRoni@users.noreply.github.com>
2021-11-29 15:04:07 +0100

    Revert "Merge branch 'main' of https://github.com/EricLocher/BringToBear"
    This reverts commit 63026cb2d936a299dc03b6c68fb28fe66ba5c1ae, reversing
    changes made to 79c5c5d23cb7ba416e4eca2304c75ca94073e29d.

--- BringToBear/Assets/Scripts/Player/PlayerMovement.cs
+++ BringToBear/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,35 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public GameObject playerShip;
     public GameObject background;
     public Rigidbody2D rb;
+
     public float stabilizeSpeed;
+    public float thrust = 200;
+    public float turnSpeed = 5;
 
-    public Vector2 lookDirection;
-    public bool boost;
+    Vector2 lookDirection;
 
-    bool driftMode;
-    float thrust = 200;
-    float turnSpeed = 5;
     float angle;
     float cameraAngle;
-    
+
+    bool boost;
+    bool driftMode;
 
     void Start()
     {
-        lookDirection = Vector2.zero;
+
     }
 
     void Update()
     {
+        Debug.Log(angle);
         driftMode = false;
 
+        lookDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * -1;
+
         Rotate();
+
+        if (Input.GetButton("Fire2")){
+            Thrust(1);
+        }
+        else if(Input.GetAxis("R2") > 0)
+        {
+            Thrust(Input.GetAxis("R2"));
+        }
+
+        if (Input.GetAxis("L2") > 0)
+        {
+            Brake();
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            Respawn();
+        }
+
+        if (Input.GetButton("Fire3"))
+        {
+            driftMode = true;
+        }
 
         if (driftMode)
         {
@@ -43,59 +67,67 @@ public class PlayerMovement : MonoBehaviour
             Stabilize();
         }
 
-       // GravityAdjuster();
+        GravityAdjuster();
 
     }
-
-    
 
     public void Rotate()
     {
+
         //TODO: Fix issue where direction gets screwy. Seems to be an issue where we need to differentiate between -180 / 180 etc.
 
         angle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
-        Quaternion newRotation = Quaternion.Euler(0, 0, 0);
+
+        Quaternion _newRotation = Quaternion.Euler(0, 0, 0);
         if (lookDirection != Vector2.zero)
         {
-            newRotation = Quaternion.Euler(new Vector3(0, 0, angle + cameraAngle));
+            _newRotation = Quaternion.Euler(new Vector3(0, 0, angle + cameraAngle));
         }
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * turnSpeed);
-    }
-
-    public void UpdateDirection(Vector2 dir)
-    {
-        lookDirection = dir;
-        Debug.Log(lookDirection);
-        Rotate();
-    }
-
-    public void SetDriftMode(bool state)
-    {
-        driftMode = state;
-    }
-
-    public bool GetDriftMode()
-    {
-        return driftMode;
+        transform.rotation = Quaternion.Slerp(transform.rotation, _newRotation, Time.deltaTime * turnSpeed);
     }
 
     public void Thrust(float thrustPower)
     {
-        if (thrustPower > 0) { thrust = 300 * thrustPower; }
-        else { thrust = 200; }
+        if (thrustPower > 0)
+        {
+            thrust = 300 * thrustPower;
+        }
+        else 
+        { 
+            thrust = 200; 
+        }
 
         rb.AddForce(transform.up * thrust * Time.deltaTime, ForceMode2D.Impulse);
     }
 
-    public void Brake(float brakePower)
+    private void Brake()
     {
-        rb.AddForce(-rb.velocity * (5 * brakePower));        
+        //rb.AddForce(Vector3.up * -100 * Time.deltaTime, ForceMode2D.Impulse);
+        rb.AddForce(-rb.velocity * 5);
+    }
+
+
+    public void Respawn()
+    {
+        transform.position = new Vector3(-10, 0, 0);
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        //GameObject newShip = Instantiate(playerShip, new Vector3(0, 0, 0), Quaternion.identity);
+        //Destroy(gameObject);
     }
 
     private void Stabilize()
     {
-        if (angle == 0 || angle == 180 || angle == -180) { return; }
-        else if ((angle < -90 && angle > -180) || (angle > 90 && angle < 180)) { return; }
+        if (angle == 0 || angle == 180 || angle == -180)
+        {
+            return;
+        }
+        else if ((angle < -90 && angle > -180) || (angle > 90 && angle < 180))
+        {
+            return;
+        }
 
         Vector2 stabilizeVector = Vector2.zero;
         Vector2 vel = rb.velocity.normalized;
@@ -113,11 +145,13 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(stabilizeVector * stabilizeSpeed * Time.deltaTime, ForceMode2D.Impulse);
 
     }
-    
+
+
     public void GravityAdjuster()
     {
         Bounds _bg = background.GetComponent<SpriteRenderer>().bounds;
         float _Posx = transform.position.x - _bg.center.x;
+
 
         if (Mathf.Abs(_Posx) > _bg.extents.x)
         {
@@ -127,7 +161,6 @@ public class PlayerMovement : MonoBehaviour
             else
                 rb.gravityScale += Time.deltaTime;
         }
-
         //TODO: Make this dynamic to the viewport rather than hard-coded y-values
         else if (transform.position.y < -12)
         {
