7d0e86aa29d514cc29e4a298f492ba2af5e642a0
EricL <eriloc1129@skola.goteborg.se>
2021-11-29 14:09:20 +0100

    Input working for 1 player

--- BringToBear/Assets/Scripts/Player/PlayerController.cs
+++ BringToBear/Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    PlayerMovement movement;
-    bool isThrust = false;
+    [SerializeField] PlayerMovement movement;
+    [SerializeField] PlayerAttack attack;
 
-
+    bool isThrust = false, isBrake = false, isAttacking = false;
     float playerDamage = 0;
 
     void Start()
@@ -18,11 +18,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (isThrust)
-            movement.UpdateThrust(1);
+            movement.Thrust(1);
+
+        if (isBrake)
+            movement.Brake(1);
+
+        if (isAttacking)
+            attack.Attack();
     }
 
+    #region Inputs
     public void Movement(InputAction.CallbackContext value)
     {
+        if(movement.GetDriftMode()) { return; }
         Vector2 _dir = value.ReadValue<Vector2>();
         _dir.x *= -1;
         movement.UpdateDirection(_dir);
@@ -30,11 +38,21 @@ public class PlayerController : MonoBehaviour
 
     public void Thrust(InputAction.CallbackContext value)
     {
-        float thrustPower = value.ReadValue<float>();
-        if(thrustPower >= 1) { isThrust = true; } 
+        float _thrustPower = value.ReadValue<float>();
+        if(_thrustPower >= 1) { isThrust = true; } 
         else { isThrust = false; }
 
-        movement.UpdateThrust(thrustPower);
+        movement.Thrust(_thrustPower);
+    }
+
+    public void Brake(InputAction.CallbackContext value)
+    {
+        
+        float _brakePower = value.ReadValue<float>();
+        if (_brakePower >= 1) { isBrake = true; }
+        else { isBrake = false; }
+
+        movement.Brake(_brakePower);
     }
 
     public void Respawn(InputAction.CallbackContext value)
@@ -45,7 +63,27 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(Vector3.zero);
     }
 
+    public void Attack(InputAction.CallbackContext value)
+    {
+        if(value.ReadValue<float>() >= 1) { isAttacking = true; }
+        else { isAttacking = false; }
 
+        attack.Attack();
+    }
+
+    public void Drift(InputAction.CallbackContext value)
+    {
+        Vector2 _dir = value.ReadValue<Vector2>();
+        _dir.x *= -1;
+
+        if (
+            _dir != Vector2.zero) { movement.SetDriftMode(true);
+            movement.UpdateDirection(_dir);
+        }
+        else { movement.SetDriftMode(false); }
+          
+    }
+    #endregion
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -62,9 +100,4 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
-
-
-
-
 }
--- BringToBear/Assets/Scripts/Player/PlayerMovement.cs
+++ BringToBear/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,57 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float thrust = 200;
-    float turnSpeed;
 
+    public GameObject playerShip;
+    public GameObject background;
+    public Rigidbody2D rb;
     public float stabilizeSpeed;
 
-    public PlayerControls input;
-
+    public Vector2 lookDirection;
     public bool boost;
-    bool driftMode;
 
+    bool driftMode;
+    float thrust = 200;
+    float turnSpeed = 5;
     float angle;
     float cameraAngle;
     
-    public Vector2 lookDirection;
-    Rigidbody2D rb;
-    public GameObject playerShip;
-    public GameObject background;
-
 
     void Start()
     {
-        turnSpeed = 5;
-        rb = GetComponent<Rigidbody2D>();
-        input = new PlayerControls();
         lookDirection = Vector2.zero;
     }
 
-    // Update is called once per frame
     void Update()
     {
         driftMode = false;
 
         Rotate();
-
-        //if (Input.GetAxis("L2") > 0)
-        //{
-        //    Brake();
-        //}
-
-
-        //if (Input.GetButton("Jump"))
-        //{
-        //    Respawn();
-        //}
-
-        //if (Input.GetButton("Fire3"))
-        //{
-
-        //    driftMode = true;
-        //}
-
 
         if (driftMode)
         {
@@ -68,13 +43,14 @@ public class PlayerMovement : MonoBehaviour
             Stabilize();
         }
 
-        GravityAdjuster();
+       // GravityAdjuster();
 
     }
 
+    
+
     public void Rotate()
     {
-
         //TODO: Fix issue where direction gets screwy. Seems to be an issue where we need to differentiate between -180 / 180 etc.
 
         angle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
@@ -93,40 +69,33 @@ public class PlayerMovement : MonoBehaviour
         Rotate();
     }
 
-    public void UpdateThrust(float thrustPower)
+    public void SetDriftMode(bool state)
     {
-        Thrust(thrustPower);
+        driftMode = state;
     }
 
-    private void Thrust(float thrustPower)
+    public bool GetDriftMode()
     {
+        return driftMode;
+    }
 
-        if (thrustPower > 0)
-        {
-            thrust = 300 * thrustPower;
-        }
-        else
-            thrust = 200;
+    public void Thrust(float thrustPower)
+    {
+        if (thrustPower > 0) { thrust = 300 * thrustPower; }
+        else { thrust = 200; }
 
         rb.AddForce(transform.up * thrust * Time.deltaTime, ForceMode2D.Impulse);
     }
 
-    private void Brake()
+    public void Brake(float brakePower)
     {
-        //rb.AddForce(Vector3.up * -100 * Time.deltaTime, ForceMode2D.Impulse);
-        rb.AddForce(-rb.velocity * 5);        
+        rb.AddForce(-rb.velocity * (5 * brakePower));        
     }
 
     private void Stabilize()
     {
-        if (angle == 0 || angle == 180 || angle == -180)
-        {
-            return;
-        }
-        else if ((angle < -90 && angle > -180) || (angle > 90 && angle < 180))
-        {
-            return;
-        }
+        if (angle == 0 || angle == 180 || angle == -180) { return; }
+        else if ((angle < -90 && angle > -180) || (angle > 90 && angle < 180)) { return; }
 
         Vector2 stabilizeVector = Vector2.zero;
         Vector2 vel = rb.velocity.normalized;
@@ -150,7 +119,6 @@ public class PlayerMovement : MonoBehaviour
         Bounds _bg = background.GetComponent<SpriteRenderer>().bounds;
         float _Posx = transform.position.x - _bg.center.x;
 
-
         if (Mathf.Abs(_Posx) > _bg.extents.x)
         {
 
@@ -159,6 +127,7 @@ public class PlayerMovement : MonoBehaviour
             else
                 rb.gravityScale += Time.deltaTime;
         }
+
         //TODO: Make this dynamic to the viewport rather than hard-coded y-values
         else if (transform.position.y < -12)
         {
