b2431c287ce29a8f7de6fedd88cd6bdb2ad190b8
Jonas Pannee <jonpan1204@skola.goteborg.se>
2021-12-02 15:52:18 +0100

    Added Damage Effects

--- BringToBear/Assets/Scripts/CollisionHandler.cs
+++ BringToBear/Assets/Scripts/CollisionHandler.cs
@@ -16,16 +16,14 @@ public class CollisionHandler : MonoBehaviour
     }
     public static void DoCollision(Rigidbody2D p1, Rigidbody2D p2)
 	{
-
 		float p1Speed = p1.velocity.magnitude;
 		float p2Speed = p2.velocity.magnitude;
 
 		Vector3 p1Angle = p1.velocity.normalized;
 		Vector3 p2Angle = p2.velocity.normalized;
 
-		float p1damagePercentage = p1.GetComponent<PlayerController>().playerDamage;
-		float p2damagePercentage = p2.GetComponent<PlayerController>().playerDamage;
-
+		float p1damagePercentage = p1.GetComponent<PlayerController>().playerDamage / 20;
+		float p2damagePercentage = p2.GetComponent<PlayerController>().playerDamage / 20;
 
 		if (p1Speed > p2Speed)
 		{
@@ -36,10 +34,10 @@ public class CollisionHandler : MonoBehaviour
 			p1Speed *= 0.5f;
 		}
 
-		p1.AddForce(p2Speed * p2Angle * p1damagePercentage);
-		p2.AddForce(p2Speed * p2Angle * p1damagePercentage);
+		p1.AddForce(p2Speed * p2Angle * p1damagePercentage, ForceMode2D.Impulse);
+		p2.AddForce(p1Speed * p1Angle * p2damagePercentage, ForceMode2D.Impulse);
 
-
+		Debug.Log(p2Speed * p2Angle * p1damagePercentage);
 	}
 
 	//GetKnockBackPercentage()
