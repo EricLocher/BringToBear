44571d9ddfbeee107a31ece095077296e680c910
Jonas Pannee <jonpan1204@skola.goteborg.se>
2021-12-06 16:04:15 +0100

    Updated Camera Behaviour

--- BringToBear/Assets/Scripts/CameraController.cs
+++ BringToBear/Assets/Scripts/CameraController.cs
@@ -17,7 +17,6 @@ public class CameraController : MonoBehaviour
 
     private float time;
     float maxTilt = 0;
-    Vector3 center;
 
     public AnimationCurve cameraAcceleration;
 
@@ -50,7 +49,6 @@ public class CameraController : MonoBehaviour
         maxTilt = Mathf.Clamp(maxTilt, 1, 60);
 
 
-        //Debug.Log(maxTilt);
 
         time += Time.deltaTime * 0.1f;
         time = Mathf.Clamp(time, 0f, 1f);
@@ -59,26 +57,68 @@ public class CameraController : MonoBehaviour
 
     public void CameraZoom()
     {
+        float minX = Players[0].transform.position.x, 
+              maxX = Players[0].transform.position.x, 
+              minY = Players[0].transform.position.y,
+              maxY = Players[0].transform.position.y;
 
-        if (Players.Count > 1)
+        foreach (PlayerController player in Players)
         {
-            float distance = Vector3.Distance(Players[0].transform.position, Players[1].transform.position);
-            float zoomLevel = Mathf.Clamp(distance, 18, 45);
-            float prevZoom = Camera.main.orthographicSize;
-            Camera.main.orthographicSize = Mathf.Lerp(prevZoom, zoomLevel, 0.01f);
+            if(player == Players[0]) { continue; }
+            Vector2 playerPos = player.transform.position;
 
-            center = ((Players[0].transform.position + Players[1].transform.position) / 2);
-            center.x = Mathf.Clamp(center.x, -12f, 12f);
+            if (playerPos.x > maxX)
+                maxX = playerPos.x;
+
+            else if (playerPos.x < minX)
+                minX = playerPos.x;
+
+            if (playerPos.y > maxY)
+                maxY = playerPos.y;
+
+            else if (playerPos.y < minY)
+                minY = playerPos.y;
         }
-        else if(Players.Count != 0)
+
+        Vector2 cameraCenter = new Vector2(((minX + maxX) / 2), ((minY + maxY) / 2));
+        float cameraSize;
+        if (maxX - minX > maxY - minY)
         {
-            center = Players[0].transform.position;
-            Camera.main.orthographicSize = 18;
-            center.x = Mathf.Clamp(center.x, -30f, 30f);
+            cameraSize = (maxX - minX) / 2;
+            cameraSize /= Camera.main.aspect;
         }
-        
-        center.y = Mathf.Clamp(center.y, -60.5f, 75f);
-        transform.position = new Vector3(center.x, center.y, -10);
+        else
+            cameraSize = (maxY - minY) / 2;
+
+        cameraSize *= 2;
+        float zoomLevel = Mathf.Clamp(cameraSize, 18, 45);
+        float prevZoom = Camera.main.orthographicSize;
+        Camera.main.orthographicSize = Mathf.Lerp(prevZoom, zoomLevel, 2f * Time.deltaTime);
+
+        cameraCenter.y = Mathf.Clamp(cameraCenter.y, -60.5f, 75f);
+        cameraCenter.x = Mathf.Clamp(cameraCenter.x, -30f, 30f);
+        transform.position = new Vector3(cameraCenter.x, cameraCenter.y, -10);
+
+
+        //if (Players.Count > 1)
+        //{
+        //    float distance = Vector3.Distance(Players[0].transform.position, Players[1].transform.position);
+        //    float zoomLevel = Mathf.Clamp(distance, 18, 45);
+        //    float prevZoom = Camera.main.orthographicSize;
+        //    Camera.main.orthographicSize = Mathf.Lerp(prevZoom, zoomLevel, 0.01f);
+
+        //    center = ((Players[0].transform.position + Players[1].transform.position) / 2);
+        //    center.x = Mathf.Clamp(center.x, -12f, 12f);
+        //}
+        //else if(Players.Count != 0)
+        //{
+        //    center = Players[0].transform.position;
+        //    Camera.main.orthographicSize = 18;
+        //    center.x = Mathf.Clamp(center.x, -30f, 30f);
+        //}
+
+        //center.y = Mathf.Clamp(center.y, -60.5f, 75f);
+        //transform.position = new Vector3(center.x, center.y, -10);
 
     }
 
