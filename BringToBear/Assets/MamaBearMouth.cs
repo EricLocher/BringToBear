6a92d43df1c3a18d03d3a758e2dd7a908c5dafe3
Jonas Pannee <jonpan1204@skola.goteborg.se>
2021-12-14 10:40:19 +0100

    Commit

---
+++ BringToBear/Assets/MamaBearMouth.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MamaBearMouth : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PlayerCoin"))
+        { 
+            //TODO: Give points to player
+        }
+    }
+}
