1453b645799a59e318ffb7f46415622b5dca40d3
Nia <90191976+PepperRoni@users.noreply.github.com>
2022-01-04 13:33:48 +0100

    Fixed stuff

--- BringToBear/Assets/Graphics/Menu/MenuController.cs
+++ BringToBear/Assets/Graphics/Menu/MenuController.cs
@@ -11,7 +11,7 @@ public class MenuController : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
     public void QuitGame()
     {
